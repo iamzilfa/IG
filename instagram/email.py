103e0a05f8ee62ab341450b7579dae50d106891d
Zilfa Cyamani <zilcyam@gmail.com>
2021-01-08 20:09:21 +0200

    modifying instagram/email.py

--- instagram/email.py
+++ instagram/email.py
@@ -4,10 +4,9 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
 
     # Creating message subject and sender
-    
+
     subject = 'Welcome to Instagram'
     sender = 'webfullstacker@gmail.com'
-
 
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name":name})
