bab5b796aea8dc7df06f19c50948171da125ac4f
Zilfa Cyamani <zilcyam@gmail.com>
2021-01-08 19:48:18 +0200

    modifying instagram/email.py

--- instagram/email.py
+++ instagram/email.py
@@ -2,7 +2,9 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
+
     # Creating message subject and sender
+    
     subject = 'Welcome to Instagram'
     sender = 'webfullstacker@gmail.com'
 
