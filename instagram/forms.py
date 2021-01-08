1ba09d8ad90bd6c60253e08adf0c5059ab209058
Zilfa Cyamani <zilcyam@gmail.com>
2021-01-08 19:08:20 +0200

    instagram/forms.py

---
+++ instagram/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Profile,Image,Comments
+
+
+class NewProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user', 'date', 'firstname', 'lastname']
+
+class NewImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['image_name', 'profile_pic', 'date', 'user', 'profile', 'likes']
+
+class commentForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['commented_image', 'posted_by', 'profile']
